9b1457469845731e6e2010e9e833c8d34b23a470
RenanFStein <renan_stein@hotmail.com>
2022-09-01 09:24:29 -0300

    Update

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -2,20 +2,12 @@
     <div class="mt-2">
         <h1 class="text-center" >Dashboard</h1>
         <div class="container ">
-             <div class="row mt-1">
-      
+             <div class="row mt-1">      
                 <div class="col-lg-12 col-md-12 col-sm-12 col-12 mt-1">
                     <input class="form-control w-100 rounded-3 " type="text" id="txtBusca" placeholder="Buscar..."/>
                 </div>
             </div>                
-                
-           
-
-            </div>
-          
-            
-          
-       
+            </div>        
         </div>
 
 </template>
@@ -32,8 +24,7 @@
             }            
             
     },  
-    async created () {
-        
+    async created () {        
             const response = await fetch("https://api-microerp.herokuapp.com/api/Caixa/")
             let req = await response.json()
             this.movimentacao = req  
