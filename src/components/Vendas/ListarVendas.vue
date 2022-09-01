3e0c69cfe63646559198e08fae438d1c6de71543
RenanFStein <renan_stein@hotmail.com>
2022-09-01 19:24:22 -0300

    update

--- src/components/Vendas/ListarVendas.vue
+++ src/components/Vendas/ListarVendas.vue
@@ -93,25 +93,7 @@
 <script>
 export default {
     methods:{
-        async updateVenda(cadastro, $event) {
-            const atualiza = {
-                "cliente": $event.target.form[0][$event.target.form[0].options.selectedIndex].__vnode.props["data-idcliente"], "estoque": {
-                    "produto": $event.target.form[1][$event.target.form[1].options.selectedIndex].__vnode.props["data-idproduto"],
-                    "quantidade": Math.abs(cadastro.quantidade),
-                    "valor": cadastro.valor
-                }
-            };
-            console.log(atualiza);
-            const requestOptions = {
-                method: "PUT",
-                headers: { "Content-Type": "application/json" },
-                body: JSON.stringify(atualiza)
-            };
-            const response = await fetch("https://api-microerp.herokuapp.com/api/Vendas/" + cadastro.id + "/", requestOptions);
-            console.log(response);
-            const data = await response.json();
-            console.log(data);
-        },
+        
         formatPrice(value) {
             let val = (value / 1).toFixed(2).replace(".", ",");
             return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
